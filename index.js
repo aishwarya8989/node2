84a9edbae154c60d5fdad971f1d84bc6ab7e0a0a
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2023-07-04 18:08:13 +0530

    submitt

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const http = require('http');
 const fs = require('fs');
+const { log } = require('console');
 
 
 const server = http.createServer((req, res) => {
-    // res.end("hyy")
-   const filePath = 'index.html';
+  // res.end("hyy")
+  const filePath = 'index.html';
 
 
-   if(req.url == "/home"){
+  if (req.url == "/home") {
     fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error');
-          return;
-        }
-        else{
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            const modifiedContent = data.replace('<%= name %>', 'John Doe');
-            res.end(modifiedContent);
-        }
-       
-      });
-   }
+      if (err) {
+        console.error(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+    
+      
+      else {
+        res.end(data)
+        // res.writeHead(200, { 'Content-Type': 'text/html' });
+        // const modifiedContent = data.replace('name', 'John Doe');
+        // res.end(modifiedContent);
+      }
+
+
+    });
+  }
+  else if (req.url == "/submit") {
+    
+    console.log(req)
+    
+
+  }
 
 
 });
@@ -31,3 +42,4 @@ const port = 8000;
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+ 
