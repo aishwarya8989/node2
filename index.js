dfe7dff60697857bfb4b6bf92a0b0046b4c2b5e9
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2023-07-04 11:40:17 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hyyy")
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+    // res.end("hyy")
+   const filePath = 'index.html';
+
+
+   if(req.url == "/home"){
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+          return;
+        }
+        else{
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            const modifiedContent = data.replace('<%= name %>', 'John Doe');
+            res.end(modifiedContent);
+        }
+       
+      });
+   }
+
+
+});
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
